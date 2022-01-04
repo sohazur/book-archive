6e61fb2fa91fbfe637908347ddebf9a1739b3fec
Sohazur <mdsohazurislam@gmail.com>
2022-01-04 08:10:19 +0400

    Added basic search box, and functioning js functions for searching from API

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Hi");
+// function for searching books
+const searchBooks = () => {
+  const searchText = document.getElementById("search-field").value;
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayBooks(data));
+};
+
+// function for displaying search results
+const displayBooks = (books) => {
+  console.log(books);
+};
